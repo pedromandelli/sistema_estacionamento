fb58acd4b079496f6d6adfeaf4a79cfd37c94c22
Pedro Mandelli <pbalconimandelli@gmail.com>
2024-05-27 20:00:37 -0300

    Create and get user

--- user_service/main.py
+++ user_service/main.py
@@ -1,34 +1,45 @@
 # user_service/main.py
+from typing import Optional, List, Dict
+
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+
 from pymongo import MongoClient
 import uvicorn
 
+from pydantic import BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
+from bson import ObjectId
+
+from typing_extensions import Annotated
+
 app = FastAPI()
 
-uri = ""
-client = MongoClient(uri)
-db = client.Cluster0
+uri = "mongodb+srv://pbalconimandelli:LtwoROD0lfsCYF7i@embarcados.soia3gj.mongodb.net/?retryWrites=true&w=majority&appName=Embarcados"
+client = MongoClient(uri, tls=True, tlsAllowInvalidCertificates=True)
+db = client.Embarcados
 users_collection = db.get_collection("users")
 
-class User(BaseModel):
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class UserModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     cpf: str
     name: str
     category: str
 
 @app.post("/users/")
-async def create_user(user: User):
-    user_dict = user.dict()
-    if users_collection.find_one({"cpf": user.cpf}):
+async def create_user(user: UserModel):
+    user_exists = users_collection.find_one({"cpf": user.cpf})
+    if user_exists:
         raise HTTPException(status_code=400, detail="User already exists")
-    users_collection.insert_one(user_dict)
-    return user_dict
+    new_user = users_collection.insert_one(user.dict())
+    return user
 
 @app.get("/users/{cpf}")
 async def get_user(cpf: str):
     user = users_collection.find_one({"cpf": cpf})
     if user:
-        return user
+        return UserModel(**user)
     raise HTTPException(status_code=404, detail="User not found")
 
 if __name__ == "__main__":
