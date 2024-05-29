06c3174fe32b7dd2cf59a6e00fe6911da52ead79
Pedro Mandelli <pbalconimandelli@gmail.com>
2024-05-28 21:37:49 -0300

    Adiciona validação de usuário ao adicionar crédito e melhora modelo de crédito

--- credit_service/main.py
+++ credit_service/main.py
@@ -1,22 +1,38 @@
 # credit_service/main.py
+from typing import Optional, List, Dict
+
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+
 from pymongo import MongoClient
 import uvicorn
+
+from pydantic import BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
+from bson import ObjectId
+
+from typing_extensions import Annotated
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 app = FastAPI()
 
 uri = "mongodb+srv://pbalconimandelli:LtwoROD0lfsCYF7i@embarcados.soia3gj.mongodb.net/?retryWrites=true&w=majority&appName=Embarcados"
-client = MongoClient(uri)
+client = MongoClient(uri, tls=True, tlsAllowInvalidCertificates=True)
 db = client.Embarcados
+users_collection = db.get_collection("users")
 credits_collection = db.get_collection("credits")
 
-class Credit(BaseModel):
+class CreditModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     cpf: str
     amount: float
 
 @app.post("/credits/")
-async def add_credit(credit: Credit):
+async def add_credit(credit: CreditModel):
+    user = users_collection.find_one({"cpf": credit.cpf})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
     user_credit = credits_collection.find_one({"cpf": credit.cpf})
     if user_credit:
         new_amount = user_credit["amount"] + credit.amount
@@ -27,9 +43,9 @@ async def add_credit(credit: Credit):
 
 @app.get("/credits/{cpf}")
 async def get_credit(cpf: str):
-    credit = credits_collection.find_one({"cpf": cpf})
-    if credit:
-        return credit
+    credit_data = credits_collection.find_one({"cpf": cpf})
+    if credit_data:
+        return CreditModel(**credit_data)
     raise HTTPException(status_code=404, detail="Credit not found")
 
 if __name__ == "__main__":
