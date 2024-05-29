0f93ee0090be2b5987d44af0f3e8f8fd278ff75a
Pedro Mandelli <pbalconimandelli@gmail.com>
2024-05-29 19:30:43 -0300

    refactor for isolating microservices and handling errors

--- credit_service/main.py
+++ credit_service/main.py
@@ -12,6 +12,8 @@ from bson import ObjectId
 
 from typing_extensions import Annotated
 
+import httpx
+
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 app = FastAPI()
@@ -29,30 +31,33 @@ class CreditModel(BaseModel):
 
 @app.post("/credits/")
 async def add_credit(credit: CreditModel):
-    user = users_collection.find_one({"cpf": credit.cpf})
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+    async with httpx.AsyncClient() as client:
+        user_response = await client.get(f"http://localhost:8001/users/{credit.cpf}")
+        if user_response.status_code != 200:
+            raise HTTPException(status_code=404, detail="User not found")
+        user = user_response.json()
 
-    user_credit = credits_collection.find_one({"cpf": credit.cpf})
-    if user_credit:
-        new_amount = user_credit["amount"] + credit.amount
-        credits_collection.update_one({"cpf": credit.cpf}, {"$set": {"amount": new_amount}})
-    else:
-        credits_collection.insert_one(credit.dict())
-    return {"cpf": credit.cpf, "amount": credit.amount}
+        user_credit = credits_collection.find_one({"cpf": credit.cpf})
+        if user_credit:
+            new_amount = user_credit["amount"] + credit.amount
+            credits_collection.update_one({"cpf": credit.cpf}, {"$set": {"amount": new_amount}})
+        else:
+            credits_collection.insert_one(credit.dict())
+        return {"cpf": credit.cpf, "amount": credit.amount}
 
 @app.get("/credits/{cpf}")
 async def get_credit(cpf: str):
     credit_data = credits_collection.find_one({"cpf": cpf})
     if credit_data:
         return CreditModel(**credit_data)
-    raise HTTPException(status_code=404, detail="Credit not found")
+    else:
+        return {"message": "User does not have credits"}
 
 @app.post("/credits/{cpf}/decrement")
 async def decrement_credits(cpf: str):
     credit = credits_collection.find_one({"cpf": cpf})
     if not credit or credit["amount"] <= 0:
-        raise HTTPException(status_code=400, detail="Insufficient credits")
+        return {"message": "User does not have enough credits"}
     credits_collection.update_one({"cpf": cpf}, {"$inc": {"amount": -1}})
     return {"message": "Credits decremented"}
 
