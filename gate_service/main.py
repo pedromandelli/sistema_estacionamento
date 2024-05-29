923939341b4308976679d747d32852e9f710283c
Pedro Mandelli <pbalconimandelli@gmail.com>
2024-05-29 15:02:04 -0300

    feat: Implementa controle de portão com ações de abrir e fechar
    Adiciona um endpoint POST /gate/ que aceita comandos para abrir ou fechar o portão. O modelo GateControlModel valida a ação recebida.

--- gate_service/main.py
+++ gate_service/main.py
@@ -1,15 +1,27 @@
 # gate_service/main.py
+from typing import Optional, List, Dict
+
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+
+from pymongo import MongoClient
 import uvicorn
+
+from pydantic import BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
+from bson import ObjectId
+
+from typing_extensions import Annotated
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 app = FastAPI()
 
-class GateControl(BaseModel):
-    action: str  # "open" or "close"
+class GateControlModel(BaseModel):
+    id: PyObjectId = Field(alias="_id", default=None)
+    action: str = Field(..., pattern="^(open|close)$")  # "open" or "close"
 
 @app.post("/gate/")
-async def control_gate(control: GateControl):
+async def control_gate(control: GateControlModel):
     if control.action == "open":
         print("Gate is opening...")
     elif control.action == "close":
@@ -17,6 +29,5 @@ async def control_gate(control: GateControl):
     else:
         raise HTTPException(status_code=400, detail="Invalid action")
     return {"message": f"Gate {control.action} command sent"}
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8005)
